8d9faff14c99b91ceb72e4490e31760fffe870f9
tutla <ayatullah_tutla@yahoo.com>
2024-11-11 12:04:11 +0700

    Solved Perknalan Sieve of Eratosthener

---
+++ pemrograman-kompetitif-dasar/02_Matematika_Diskret_Dasar/perkenalan-sieve-of-eratosthenes/src/main.rs
@@ -0,0 +1,50 @@
+#![allow(unused)]
+
+use std::io::stdin;
+
+const N: usize = 1000000;
+
+fn take_int() -> usize {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    return input.trim().parse().unwrap();
+}
+
+fn sieve(sieve_array: &mut Vec<bool>, n: usize){
+    sieve_array[0] = false;
+    sieve_array[1] = false;
+
+    for i in (4..n+1).step_by(2) { sieve_array[i] = false; }
+
+    let mut i = 3;
+    while i*i<= n+1 {
+        if sieve_array[i] {
+            for j in (i*i..n+1).step_by(2*i){ 
+                sieve_array[j] = false; 
+            }
+        }
+        i+=2;
+    }
+}
+
+fn main() {
+    let mut is_prime: Vec<bool> = vec![true; N+1];
+    let mut prime: [usize;N] = [0;N];
+    let mut idx: usize = 0;
+
+    sieve(&mut is_prime, N);
+
+    let test_case = take_int();
+
+    for i in 0..N {
+		if is_prime[i]==true {
+			prime[idx]=i;
+			idx +=1;
+		}
+	}    
+
+    for i in 0..test_case {
+    	let x = take_int();
+    	println!("{}",prime[x-1]);
+    }
+}

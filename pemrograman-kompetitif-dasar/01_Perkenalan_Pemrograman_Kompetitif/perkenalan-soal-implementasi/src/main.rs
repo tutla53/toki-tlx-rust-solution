383d932e8b84a0b126ce812688ef7ad7b0e95c04
Tutla Ayatullah <39817427+tutla53@users.noreply.github.com>
2024-11-13 06:35:51 +0700

    Solved: Perkenalan Soal Implementasi

--- pemrograman-kompetitif-dasar/01_Perkenalan_Pemrograman_Kompetitif/perkenalan-soal-implementasi/src/main.rs
+++ pemrograman-kompetitif-dasar/01_Perkenalan_Pemrograman_Kompetitif/perkenalan-soal-implementasi/src/main.rs
@@ -2,31 +2,35 @@
 
 use std::io::stdin;
 
-fn take_int() -> usize {
+fn take_vector() -> Vec<u8> {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
-    return input.trim().parse().unwrap()
-}
-
-fn take_vector() -> Vec<usize> {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    let arr: Vec<usize> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
+    let arr: Vec<u8> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
     return arr;
 }
 
-fn take_string() -> Vec<char> {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    let vec:Vec<char> = input.trim().chars().collect();
-    return vec;
-}
-
-fn to_string(vec:Vec<char>) -> String{
-    return vec.iter().collect::<String>();
-}
-
 fn main(){
-    let t = take_int() as u32;
-    println!("{}", t*2);
+    let mut arr: Vec<Vec<u8>> = vec![vec![0; 100]; 100];
+    let mut rotate: Vec<Vec<u8>> = vec![vec![0; 100]; 100];
+
+    let dimension = take_vector();
+    let row = dimension[0] as usize;
+    let col = dimension[1] as usize;
+
+    for i in 0..row{
+        arr[i] = take_vector();
+    }
+    
+    for i in 0..row{
+        for j in 0..col{
+            rotate[j][row-i-1] = arr[i][j];
+        }
+    }
+
+    for i in 0..col{
+        for j in 0..row-1{
+            print!("{} ", rotate[i][j]);
+        }
+        println!("{}", rotate[i][row-1]);
+    }
 }

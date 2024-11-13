8ea6f5f3f3fe089ac02fe796e022cf709f2592f4
tutla <ayatullah_tutla@yahoo.com>
2024-11-13 09:28:01 +0700

    Update Input take_int and take_vector to generic type

--- pemrograman-kompetitif-dasar/01_Perkenalan_Pemrograman_Kompetitif/perkenalan-soal-implementasi/src/main.rs
+++ pemrograman-kompetitif-dasar/01_Perkenalan_Pemrograman_Kompetitif/perkenalan-soal-implementasi/src/main.rs
@@ -1,11 +1,15 @@
 #![allow(unused)]
 
-use std::io::stdin;
+use {
+    std::io::stdin,
+    std::str::FromStr,
+    std::fmt::Debug
+};
 
-fn take_vector() -> Vec<u8> {
+fn take_vector<T: FromStr>() -> Vec<T> where <T as FromStr>::Err: Debug{
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
-    let arr: Vec<u8> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
+    let arr: Vec<T> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
     return arr;
 }
 
@@ -13,9 +17,9 @@ fn main(){
     let mut arr: Vec<Vec<u8>> = vec![vec![0; 100]; 100];
     let mut rotate: Vec<Vec<u8>> = vec![vec![0; 100]; 100];
 
-    let dimension = take_vector();
-    let row = dimension[0] as usize;
-    let col = dimension[1] as usize;
+    let dimension: Vec<usize> = take_vector();
+    let row = dimension[0];
+    let col = dimension[1];
 
     for i in 0..row{
         arr[i] = take_vector();
--- template/src/main.rs
+++ template/src/main.rs
@@ -1,17 +1,21 @@
 #![allow(unused)]
 
-use std::io::stdin;
+use {
+    std::io::stdin,
+    std::str::FromStr,
+    std::fmt::Debug
+};
 
-fn take_int() -> usize {
+fn take_int<T: FromStr>() -> T where <T as FromStr>::Err: Debug{
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
     return input.trim().parse().unwrap()
 }
 
-fn take_vector() -> Vec<usize> {
+fn take_vector<T: FromStr>() -> Vec<T> where <T as FromStr>::Err: Debug{
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
-    let arr: Vec<usize> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
+    let arr: Vec<T> = input.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
     return arr;
 }
 
@@ -27,6 +31,6 @@ fn to_string(vec:Vec<char>) -> String{
 }
 
 fn main(){
-    let t = take_int() as u32;
+    let t:u32 = take_int();
     println!("{}", t*2);
 }
